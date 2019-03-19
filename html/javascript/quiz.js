f612fe3ebcd25339c754820eec8c2b8b9606b7fb
FiftyBillion <pianoboy1103@gmail.com>
2019-03-19 13:26:20 -0700

    final update

--- html/javascript/quiz.js
+++ html/javascript/quiz.js
@@ -12,8 +12,8 @@ function init() {
     width: 0,
     height: 0
   } );
-  $('#message').hide();
-  $('#message').css( {
+  $('.message').hide();
+  $('.message').css( {
     left: '580px',
     top: '250px',
     width: 0,
@@ -23,20 +23,21 @@ function init() {
   
   // Reset the game
   correctMatches = 0;
-  var numOfMatches=5;
+  var numOfMatches=6;
   
   $('#draggableBlocks').html( '' );
   
   $('#targetArea').html( '' );
   
   // Create the draggable blocks (Shuffled after every reset)
-  var numbers = [ 1, 2, 3, 4, 5];
+  var numbers = [ 1, 2, 3, 4, 5, 6];
   
   var text = [ 'Run', 
   'Active Assailiant',
   'Help lower the body count by equipping people with the latest life-saving preparedness training.',
   'Because you will recognize when your survival instinct is taking over your thinking brain.',
-  'An individual actively engaged in killing or attempting to kill people in a confined and populated area.'
+  'An individual actively engaged in killing or attempting to kill people in a confined and populated area.',
+  'Try to communicate with police silently.'
   ];
   
  numbers.sort( function() { return Math.random() - .5 } );
@@ -56,7 +57,7 @@ function init() {
   'What is another term that can be used to describe an active shooter?',
   'What is the goal of active shooter preparedness?',
   'Why is knowing your natural stress response key to surviving an active shooter event?', 
-  'Define Active Shooter.'];
+  'Define Active Shooter.', 'What could you be doing when hiding from an active shooter?'];
   
   for ( var i=1; i<=words.length; i++ ) {
     $('<div>' + words[i-1] + '</div>').data( 'number', i ).appendTo( '#targetArea' ).droppable( {
@@ -72,6 +73,7 @@ function handleDrop( event, ui ) {
 
   var targetNumber = $(this).data( 'number' );
   var draggableBlockNumber = ui.draggable.data( 'number' );
+  var audio = new Audio('sounds/sound.mp3');
 
   // If the card was dropped to the correct slot,
   // change the card colour, position it directly
@@ -81,41 +83,30 @@ function handleDrop( event, ui ) {
  if ( targetNumber == draggableBlockNumber ) {
  //provide some related facts
  if(targetNumber==1){
-  $('#message').show().html("Correct. The first thing you want to do is run away from the situation to a safe place before proceeding with anything else. Learn more here: https://www.ready.gov/active-shooter");
+  $('.message').show().html("Correct. The first thing you want to do is run away from the situation to a safe place before proceeding with anything else. Learn more here: https://www.ready.gov/active-shooter");
+  audio.play();
   } 
   if(targetNumber==2){
-  $('#message').show().html("Correct. Other terms for an active shooter include Active Killer and Violent Intruder. Learn more here: https://www.alicetraining.com/active-shooter/");
+  $('.message').show().html("Correct. Other terms for an active shooter include Active Killer and Violent Intruder. Learn more here: https://www.alicetraining.com/active-shooter/");
+  audio.play();
   } 
   if(targetNumber==3){
-  $('#message').show().html("Correct. The goal of this training is to help you and keep others around you safe.Learn more here: https://vividlearningsystems.com/active-shooter-preparedness");
+  $('.message').show().html("Correct. The goal of this training is to help you and keep others around you safe. Learn more here: https://vividlearningsystems.com/active-shooter-preparedness");
+  audio.play();
   } 
   if(targetNumber==4){
-  $('#message').show().html("Correct. It is important to think rationally in these types of situations in order to be safe.");
-	 } 
+  $('.message').show().html("Correct. It is important to think rationally in these types of situations in order to be safe.");
+  audio.play();
+  } 
   if(targetNumber==5){
-  $('#message').show().html("Correct. The definition of active shooter is an agreed-upon definition by US government agencies, including the White House, US Department of Justice, FBI, US Department of Education, US Department of Homeland Security, and Federal Emergency Management Agency.");
+  $('.message').show().html("Correct. The definition of active shooter is an agreed-upon definition by US government agencies, including the White House, US Department of Justice, FBI, US Department of Education, US Department of Homeland Security, and Federal Emergency Management Agency.");
+  audio.play();
   } 
- /* if(targetNumber==6){
-  $('#message').show().html("");
+    if(targetNumber==6){
+  $('.message').show().html("Correct. You can sse text message or social media to tag your location, or put a sign in a window while hiding during the situation. Learn more here: https://www.ready.gov/active-shooter");
+  audio.play();
   } 
-  if(targetNumber==7){
-  $('#message').show().html("");
-  } 
-  if(targetNumber==8){
-  $('#message').show().html("");
-  } 
-  if(targetNumber==9){
-  $('#message').show().html("");
-  } 
-  if(targetNumber==10){
-  $('#message').show().html("");
-  } 
-  if(targetNumber==11){
-  $('#message').show().html("");
-  } 
-  if(targetNumber==12){
-  $('#message').show().html("");
-  } */
+
   
   animateMessage();
     ui.draggable.addClass( 'correct' );
@@ -130,12 +121,12 @@ function handleDrop( event, ui ) {
   // If all the blocks have been placed correctly then display a message
   // and reset the blocks for another go
  
-  if ( correctMatches == 5 ) {
+  if ( correctMatches == 6 ) {
     $('#successMessage').show();
     $('#successMessage').animate( {
-      left: '380px',
-      top: '200px',
-      width: '400px',
+	  left: '80px',
+      top: '490px',
+      width: '450px',
       height: '100px',
       opacity: 1
     } );
@@ -143,10 +134,10 @@ function handleDrop( event, ui ) {
  
 }
 function animateMessage(){
-$('#message').animate( 
+$('.message').animate( 
     {
-      left: '180px',
-      top: '200px',
+      left: '80',
+      top: '700px',
       width: '600px',
       height: '100px',
       opacity: 1
@@ -154,7 +145,7 @@ $('#message').animate(
      
      }
 function hideMessage(){
-$('#message').animate( 
+$('.message').animate( 
     {
       left: '220px',
       top: '700px',
